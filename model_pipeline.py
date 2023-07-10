83e677a682722521082050217531e58044b527fd
Ayush Singh <theayushsingh72@gmail.com>
2023-07-10 07:31:41 +0530

    Added Files:Base SD2 for reference

---
+++ model_pipeline.py
@@ -0,0 +1,16 @@
+##all imports go here
+
+import torch
+
+
+## all from imports go here
+from diffusers import StableDiffusionPipeline
+from diffusers import EulerDiscreteScheduler      #### V2 has a different scheduler
+
+
+
+
+
+
+class TextToVideoPipeline(StableDiffusionPipeline):
+    def init
